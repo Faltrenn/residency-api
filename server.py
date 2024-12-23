78ce83363d95f8be22caf42b3394a720bfc87d98
manel <manelzaum@icloud.com>
2024-12-23 11:05:27 -0300

    add: warning to bad usage.

--- server.py
+++ server.py
@@ -11,9 +11,13 @@ class RequestHandler(BaseHTTPRequestHandler):
 
 if __name__ == "__main__":
     server = HTTPServer((argv[1], int(argv[2])), RequestHandler)
-    print(f"Server started at {argv[1]}:{argv[2]}")
-    try:
-        server.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    server.server_close()
+    if len(argv) != 3 or not argv[2].isnumeric():
+        print("WRONG USAGE!")
+        print("server.py [ip] [port]")
+    else:
+        print(f"Server started at {argv[1]}:{argv[2]}")
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            pass
+        server.server_close()
