370b7ffdee1be92074d95f5566731a3259e9ff6f
manel <manelzaum@icloud.com>
2024-12-29 00:07:47 -0300

    add: intitution to db.

--- create_db.sql
+++ create_db.sql
@@ -9,3 +9,16 @@ INSERT INTO
 VALUES
   ("Professor"),
   ("Residente");
+
+CREATE TABLE IF NOT EXISTS institution (
+  id AUTO_INCREMENT SMALLINT,
+  name VARCHAR(50),
+  short_name VARCHAR(10),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO
+  intitution (name, short_name)
+VALUES
+  ("Escola Multicampi de Ciências Médicas", "EMCM"),
+  ( "Universidade Federal do Rio Grande do Norte", "UFRN", "asdasd", "asdasdasdassd", "asdasdasdada")
